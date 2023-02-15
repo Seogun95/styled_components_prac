7bed5009e6157723cced96260e3c87a0afbce2ed
Seogun95 <ksj083895@gmail.com>
2023-02-15 23:35:06 +0900

    style: Theme 생성

---
+++ src/styles/Theme.jsx
@@ -0,0 +1,29 @@
+export const Color = {
+  white: '#ffffff',
+  black: '#000000',
+  tomato: 'tomato',
+  brandColor: '#32f9c0',
+  trans: 'transparent',
+};
+
+export const FontSize = {
+  large: '3rem',
+  medium: '1.75rem',
+  defalut: '1.125rem',
+  small: '1rem',
+  micro: '0.875rem',
+};
+
+export const FontWeight = {
+  weight700: 700,
+  default: 600,
+  weight400: 400,
+};
+
+export const BorderRadius = {
+  round: '100%',
+  half_round: '50%',
+  large: '3rem',
+  defalut: '1.5rem',
+  small: '0.5rem',
+};
