bd880c494007e9f4e7a3beaa89b4c1508c944e67
Seogun95 <ksj083895@gmail.com>
2023-02-16 02:38:17 +0900

    feat: InputBox 컴포넌트를 통해 input을 세자리 단위로 끊기

---
+++ src/components/InputBox.jsx
@@ -0,0 +1,61 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import Input from '../common/Input';
+import Button from '../common/Button';
+import { Color } from '../styles/Theme';
+
+const InputContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: flex-end;
+  gap: 1rem;
+`;
+
+function InputBox() {
+  const [inputData, setInputData] = useState({
+    name: '',
+    price: 0,
+  });
+
+  const titleInputHandler = (e) => {
+    setInputData({ ...inputData, name: e.target.value });
+  };
+
+  const priceInputHandler = (e) => {
+    const value = e.target.value;
+    const removedCommaValue = Number(value.replaceAll(',', ''));
+
+    setInputData({ ...inputData, price: removedCommaValue.toLocaleString() });
+  };
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+    if (inputData.name !== '' && inputData.price !== '') {
+      alert(`{name: ${inputData.name}, price: ${inputData.price}}`);
+      console.log(~~inputData.price.split(',').join(''));
+    } else alert('이름과 가격 모두 입력해주세요.');
+  };
+
+  return (
+    <>
+      <h1>Input</h1>
+      <InputContainer>
+        <Input
+          label={'이름'}
+          value={inputData.name}
+          onChange={titleInputHandler}
+        />
+        <Input
+          label={'가격'}
+          value={inputData.price}
+          onChange={priceInputHandler}
+        />
+        <Button middle color={Color.black} onClick={onSubmitHandler}>
+          저장
+        </Button>
+      </InputContainer>
+    </>
+  );
+}
+
+export default InputBox;
